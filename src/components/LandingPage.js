1e4f6f84483931b802ac7befe02399f4d76e1b38
yazanismail1 <yalfarra@outlook.com>
2023-01-14 11:03:10 +0000

    feat: added bootstrap library

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,24 +1,25 @@
-import React from "react";
-import Header from "./components/Header";
-import Introduction from "./components/Introduction";
-import AboutUs from "./components/AboutUs";
-import Services from "./components/Services";
-import Tuition from "./components/Tuition";
-import Curriculum from "./components/Curriculum";
-import ContactUs from "./components/ContactUs";
-import Footer from "./components/Footer";
+"use client";
+
+import Header from './Header'
+// import Introduction from "./components/Introduction";
+// import AboutUs from "./components/AboutUs";
+// import Services from "./components/Services";
+// import Tuition from "./components/Tuition";
+// import Curriculum from "./components/Curriculum";
+// import ContactUs from "./components/ContactUs";
+// import Footer from "./components/Footer";
 
 export default function LandingPage() {
   return (
     <>
       <Header />
-      <Introduction />
+      {/* <Introduction />
       <AboutUs />
       <Services />
       <Tuition />
       <Curriculum />
       <ContactUs />
-      <Footer />
+      <Footer /> */}
     </>
   );
 }
