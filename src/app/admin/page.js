93b2a76656922cd243aec3fa9fab4e0adf8ebe1a
omarali1997 <omar.aziz.9718@gmail.com>
2023-01-16 20:21:13 +0000

    adminPanel CreateCourse

--- src/app/admin/page.js
+++ src/app/admin/page.js
@@ -1,11 +1,13 @@
 import CreateUser from "./CreateUser";
+import CreateCourse from "./CreateCourse";
 import "../../styles/admin.css"
 
 export default function AdminView() {
   return (
     <>
       <section className="admin-grid">
-        <p className="admin-grid-2">ddd</p>
+        {/* <p className="admin-grid-2">ddd</p> */}
+        <CreateCourse/>
         <CreateUser />
       </section>
     </>
