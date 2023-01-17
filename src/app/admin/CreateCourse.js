ba95a3bd6407ed03a73851a4ceaf99348d873e0a
yazanismail1 <yalfarra@outlook.com>
2023-01-17 15:23:08 +0000

    feat

--- src/app/admin/CreateCourse.js
+++ src/app/admin/CreateCourse.js
@@ -135,7 +135,8 @@ export default function CreateCourse(props) {
                                 type="time"
                                 name="time"
                                 id="time"
-                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                className="w-full rounded-md border border-[#e0e0e0] bg-white py-3 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                                onChange={(e) => console.log(e)}
 
                             />
                         </div>
