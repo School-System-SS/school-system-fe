fe33e9826268d4560410b537c6882c066d0be65b
mohannadghbashneh <ghbashneh99@gmail.com>
2023-01-17 15:17:06 +0200

    feat:add teacherHeader,styling to GradeBook page by Mohannad , Lujain

---
+++ src/app/teacher/Dashboard/LogoutButton.js
@@ -0,0 +1,6 @@
+import React from 'react'
+
+export default function LogoutButton() {
+    return <a href="/signin" className="bg-lighter py-2 px-4 rounded text-white lg:mt-0 md:mt-2 lg:w-fit md:w-full max-[640px]:mt-2 max-[640px]:w-full sm:w-full hover:bg-darker">Logout</a>;
+
+}
--- src/app/teacher/page.js
+++ src/app/teacher/page.js
@@ -1,12 +1,18 @@
 "use client"
 import React from 'react'
-import ListViewAssignment from '../teacher/teacherDashboard/ListViewAssignment'
+import "src/styles/general.css";
 
+import ListViewAssignment from './Dashboard/ListViewAssignment'
+import TeacherDashboardHeader from './Dashboard/TeacherDashboardHeader'
 export default function Page() {
 
     return (
-        <>
+        <div class="teacher-page">
+            
+
+            <TeacherDashboardHeader/>
             <ListViewAssignment />
-        </>
+        </div>
+        
     )
 }
