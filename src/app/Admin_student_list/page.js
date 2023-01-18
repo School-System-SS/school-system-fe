c852acb2bee78f7bdc75acc548f70246ce1be3d8
Lujain92 <lujainaljarrah92@gmail.com>
2023-01-18 23:55:29 +0200

    feat: add styling to list_table_student_of_admin

---
+++ src/app/Admin_student_list/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Table_student_list from './Table_student_list'
+import AdminHeader from "../admin/adminHeader"
+export default function Page() {
+    
+
+    return (
+        <>
+         <AdminHeader/>
+        <Table_student_list/>
+        </>
+    )
+}
