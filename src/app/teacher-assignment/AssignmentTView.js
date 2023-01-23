d3d1d6b7a99ae226620a2d8c541a7a9dad46b556
omarali1997 <omar.aziz.9718@gmail.com>
2023-01-23 14:45:29 +0000

    dd

--- src/app/teacher-assignment/AssignmentTView.js
+++ src/app/teacher-assignment/AssignmentTView.js
@@ -38,7 +38,7 @@ export default function AssignmentTeacherView() {
 
   return (
 
-    <section className="TeacherAssignment-2 ">
+    <section className="">
       <div className="mx-24 flex flex-col ">
         <div className="overflow-x-auto sm:mx-0.5 lg:mx-0.5">
           <div className="py-2 inline-block min-w-full sm:px-8 lg:px-8">
--- src/app/teacher-assignment/CreateAssignment.js
+++ src/app/teacher-assignment/CreateAssignment.js
@@ -1,8 +1,11 @@
 import React, { useState } from "react";
-import "../../styles/teacher.css";
 import axios from "axios";
+import { Side } from "../teacher-dashboard/Side";
+import AssignmentTeacherView from "./AssignmentTView";
+import { useRouter } from "next/navigation";
 
 export default function Create_assignment() {
+  const router = useRouter();
 
   const createOneAssignment = (e) => {
     e.preventDefault()
@@ -16,85 +19,127 @@ export default function Create_assignment() {
       title: e.target.Title.value,
       description: e.target.message.value,
       due_date: e.target.DueDate.value,
-      points: 10,
-      course: 1,
+      points: e.target.points.value,
+      course: JSON.parse(localStorage.getItem("courseId")),
       teacher: JSON.parse(localStorage.getItem("userId"))
     };
+    console.log(body);
 
     axios
-      .post(URL, body,config)
+      .post(URL, body, config)
       .then((res) => {
-    const STUURL = 'https://school-system-final-project.herokuapp.com/api/v1/studentAssignment/create/';
+        const STUURL = 'https://school-system-final-project.herokuapp.com/api/v1/studentAssignment/create/';
         axios
-      .post(STUURL, {course:1,assignment:res.data.pk},config)
-      .then((res1) => {
-        alert("Create Successfully");
+          .post(STUURL, { course: 1, assignment: res.data.pk }, config)
+          .then((res1) => {
+            alert("Create Successfully");
+          })
+          .catch((err) => {
+            alert(err, "error");
+          })
       })
       .catch((err) => {
-        alert(err,"error");
-      })
-      })
-      .catch((err) => {
-        alert(err,"error");
+        alert(err, "error");
       })
   }
 
   return (
 
-    <section className="TeacherAssignment-1">
+    <section className="flex">
+      <section className="h-full w-[30%] flex flex-row">
+        <Side />
+      </section>
+      <section className="w-full ">
 
-      <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
-        <div className="flex flex-col justify-between ">
-          <div className="space-y-2">
-            <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Create assignment</h2>
+        <div className=" ml-[5%] grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 rounded-lg md:grid-cols-2 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
+          <div className="flex flex-col justify-between ">
+            <div className="space-y-2">
+              <h2 className="text-4xl ml-[5%] font-bold leading-tight lg:text-5xl">Create assignment</h2>
+            </div>
+            <img src="https://mambaui.com/assets/svg/doodle.svg" alt="" className=" h-52 md:h-64" />
           </div>
-          <img src="https://mambaui.com/assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
-        </div>
-        <div className="rounded bg-white max-w-md  overflow-hidden shadow-xl p-9 content-center">
-          <form className="space-y-4" onSubmit={createOneAssignment} >
-            <input type="hidden" name="remember" value="True" />
-            <div className="rounded-md shadow-sm -space-y-px">
-              <div className="grid gap-6">
-                <div className="col-span-12">
-                  <input
-                    type="text"
-                    name="Title"
-                    id="Title"
-                    placeholder="Title"
-                    className="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-lg border-gray-300 rounded-md"
-                  />
-                </div>
-                <div className="col-span-12">
-                  <input
-                    type="date"
-                    name="DueDate"
-                    id="DueDate"
-                    placeholder="Due Date"
-                    className=" focus:ring-indigo-700 focus:border-indigo-700 block w-full  sm:text-lg border-gray-600 rounded-md"
-                  />
+          <div className="relative  sm:rounded-3xl  ">
+            <form className="space-y-4" onSubmit={createOneAssignment} >
+              <input type="hidden" name="remember" value="True" />
+              <div className="rounded-md shadow-sm -space-y-px">
+                <div className="grid gap-6">
+                  <div className="col-span-12">
+                    <label
+                      for="name"
+                      className="block mb-2 text-base font-medium text-darker"
+                    >
+                      Assignment Title
+                    </label>
+                    <input
+                      type="text"
+                      name="Title"
+                      id="Title"
+                      placeholder="Title"
+                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    />
+                  </div>
+                  <div className="col-span-12">
+                    <label
+                      for="name"
+                      className="block mb-2 text-base font-medium text-darker"
+                    >
+                      Due Date
+                    </label>
+                    <input
+                      type="date"
+                      name="DueDate"
+                      id="DueDate"
+                      placeholder="Due Date"
+                      className=" w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    />
+                  </div>
+                  <div className="col-span-12">
+                    <label
+                      for="name"
+                      className="block mb-2 text-base font-medium text-darker"
+                    >
+                      Points
+                    </label>
+                    <input
+                      type="number"
+                      name="points"
+                      id="points"
+                      placeholder="10/10"
+                      className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                    />
+                  </div>
                 </div>
               </div>
-            </div>
-            <div>
-              <textarea
-                id="message"
-                name="message"
-                rows="3"
-                className="w-full rounded dark:bg-gray-800"
-                placeholder="Description"
-              />
-            </div>
-            <div>
+              <div>
+                <label
+                  for="name"
+                  className="block mb-2 text-base font-medium text-darker"
+                >
+                  Description
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows="3"
+                  className="w-full rounded-md border border-[#e0e0e0] bg-white py-2 px-6 text-base font-medium text-[#6B7280] outline-none focus:border-[#6A64F1] focus:shadow-md"
+                  placeholder="Description"
+                />
+              </div>
+
               <button
-                className="bg-darker text-[#FFFFFF] py-2 px-4 rounded text-white lg:mt-0 md:mt-2 lg:w-fit md:w-full max-[640px]:mt-2 max-[640px]:w-full sm:w-full hover:bg-main"
+                className="w-full px-8 py-2 mt-4 text-base font-semibold text-[#FFFFFF] rounded-md outline-none hover:shadow-form bg-main hover:bg-lighter"
                 type="submit"
                 id="btnsubmit">
                 Assign
               </button>
-            </div>
-          </form>
+
+            </form>
+          </div>
         </div>
-      </div>
+
+        <AssignmentTeacherView />
+      </section>
+
     </section>
   );
 }
